b10f0dd98533cf2a034d4bf8bee7a9c8887c4a78
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2025-12-01 13:01:10 +0000

    fix custom gita commands

--- .devcontainer/install_gita.sh
+++ .devcontainer/install_gita.sh
@@ -9,6 +9,7 @@ sudo apt install -y pipx
 pipx install gita
 
 # Enable bash autocompletion for gita
+echo -e "\n# S-CORE gita setup" >> ~/.bashrc
 echo "eval \"\$(register-python-argcomplete gita -s bash)\"" >> ~/.bashrc
 
 # Set GITA_PROJECT_HOME environment variable
@@ -16,6 +17,9 @@ echo "export GITA_PROJECT_HOME=$(pwd)/.gita" >> ~/.bashrc
 GITA_PROJECT_HOME=$(pwd)/.gita
 mkdir -p "$GITA_PROJECT_HOME"
 export GITA_PROJECT_HOME
+
+# Make scripts available globally available
+echo "export PATH=\$PATH:$GITA_PROJECT_HOME/scripts" >> ~/.bashrc
 
 # Automagically clone repositories listed in baseline.csv
 gita clone --preserve-path --from-file $(pwd)/baseline.csv
